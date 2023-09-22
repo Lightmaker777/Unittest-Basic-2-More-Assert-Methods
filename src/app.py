0df27746a9bda2c9f887b9b8d7369758c73ece91
Lightmaker777 <j.kochkorian@gmx.de>
2023-09-22 14:54:56 +0200

    added solutions

--- src/app.py
+++ src/app.py
@@ -2,12 +2,14 @@ from random import randrange
 
 # function will return random number between start and end parameter where end is included
 def rnd(start, end):
-    return randrange(start, end+1)
+    return randrange(start, end + 1)  # Include 'end' in the range
 
-# function should return the greatest number in a list
-def max_num_in_list( list ):
-    max = list[ 0 ]
-    for a in list:
-        if a < max:
-            max = a
-    return max
+# function should return the highest number in a list
+def max_num_in_list(numbers):
+    if not numbers:
+        return None  # Return None for an empty list
+    max_value = numbers[0]
+    for num in numbers:
+        if num > max_value:
+            max_value = num
+    return max_value
